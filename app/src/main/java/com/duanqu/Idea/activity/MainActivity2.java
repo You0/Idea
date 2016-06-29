a210bb8ce66d17c3f1e2936254b0d0d928ad422c
You0 <1109245765@qq.com>
2016-06-29 17:21:22 +0800

    加入自定义的视频播放器
    如题

---
+++ app/src/main/java/com/duanqu/Idea/activity/MainActivity2.java
@@ -0,0 +1,122 @@
+package com.duanqu.Idea.activity;
+
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+
+import com.duanqu.Idea.R;
+import com.duanqu.Idea.player.util.DensityUtil;
+import com.duanqu.Idea.player.view.MediaController;
+import com.duanqu.Idea.player.view.SuperVideoPlayer;
+
+/**
+ * 演示demo
+ */
+public class MainActivity2 extends AppCompatActivity implements View.OnClickListener {
+    private SuperVideoPlayer mSuperVideoPlayer;
+    private View mPlayBtnView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mSuperVideoPlayer = (SuperVideoPlayer) findViewById(R.id.video_player_item_1);
+        mPlayBtnView = findViewById(R.id.play_btn);
+        mPlayBtnView.setOnClickListener(this);
+        mSuperVideoPlayer.setVideoPlayCallback(mVideoPlayCallback);
+    }
+
+    /**
+     * 播放器的回调函数
+     */
+    private SuperVideoPlayer.VideoPlayCallbackImpl mVideoPlayCallback = new SuperVideoPlayer.VideoPlayCallbackImpl() {
+        /**
+         * 播放器关闭按钮回调
+         */
+        @Override
+        public void onCloseVideo() {
+            mSuperVideoPlayer.close();//关闭VideoView
+            mPlayBtnView.setVisibility(View.VISIBLE);
+            mSuperVideoPlayer.setVisibility(View.GONE);
+            resetPageToPortrait();
+        }
+
+        /**
+         * 播放器横竖屏切换回调
+         */
+        @Override
+        public void onSwitchPageType() {
+            if (getRequestedOrientation() == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                mSuperVideoPlayer.setPageType(MediaController.PageType.SHRINK);
+            } else {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                mSuperVideoPlayer.setPageType(MediaController.PageType.EXPAND);
+            }
+        }
+
+        /**
+         * 播放完成回调
+         */
+        @Override
+        public void onPlayFinish() {
+
+        }
+    };
+
+    @Override
+    public void onClick(View view) {
+        mPlayBtnView.setVisibility(View.GONE);
+        mSuperVideoPlayer.setVisibility(View.VISIBLE);
+        mSuperVideoPlayer.setAutoHideController(false);
+        Uri uri = Uri.parse("http://115.159.159.65:8080/EAsy/test.mp4");
+        mSuperVideoPlayer.loadAndPlay(uri,0);
+    }
+
+    /***
+     * 旋转屏幕之后回调
+     *
+     * @param newConfig newConfig
+     */
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (null == mSuperVideoPlayer) return;
+        /***
+         * 根据屏幕方向重新设置播放器的大小
+         */
+        if (this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            getWindow().getDecorView().invalidate();
+            float height = DensityUtil.getWidthInPx(this);
+            float width = DensityUtil.getHeightInPx(this);
+            mSuperVideoPlayer.getLayoutParams().height = (int) width;
+            mSuperVideoPlayer.getLayoutParams().width = (int) height;
+        } else if (this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            final WindowManager.LayoutParams attrs = getWindow().getAttributes();
+            attrs.flags &= (~WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            getWindow().setAttributes(attrs);
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+            float width = DensityUtil.getWidthInPx(this);
+            float height = DensityUtil.dip2px(this, 200.f);
+            mSuperVideoPlayer.getLayoutParams().height = (int) height;
+            mSuperVideoPlayer.getLayoutParams().width = (int) width;
+        }
+    }
+
+    /***
+     * 恢复屏幕至竖屏
+     */
+    private void resetPageToPortrait() {
+        if (getRequestedOrientation() == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            mSuperVideoPlayer.setPageType(MediaController.PageType.SHRINK);
+        }
+    }
+
+}
